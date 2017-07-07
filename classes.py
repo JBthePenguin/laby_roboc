94a45524cffcae05b165554f91e2cf1479a252ad
JBthePenguin <jb_the_penguin@tutanota.com>
2017-07-07 09:21:43 +0200

    script fonctionnel, modifs faites pour avoir un script plus simple, clair, manque toujours la sauvegarde

--- classes.py
+++ classes.py
@@ -1,101 +1,117 @@
-class Carte():
-	"""docstring for Carte : classe qui produit
-	des cartes de labyrinthe qui s'affiche
-	sous forme de chaine de caractères """
-	def __init__(self, nom_du_fichier):
-		with open("cartes/{}".format(nom_du_fichier), "r") as labyrinthe_file:
-			laby = labyrinthe_file.read()
+import time
 
-		self.affichage = laby.strip()
+class Partie():
+    """docstring for Partie : classe d'objet avec les attributs:
+    - self.affichage : labyrinthe qui s'affiche sous forme de chaine de caractères
+    - self.nbre_ligne : nombre de lignes du labyrinthe -> y
+    - self.nbre_caractere_par_ligne : nombre de caractères par ligne -> x
+    - self.caractere_a_ajouter : caractère à ajouter à la place de Roboc (X) après un déplacement"""
+    def __init__(self, nom_du_fichier):
+        """fonction de construction de l'objet et de ses attributs
+        prenant en paramètre le fichier contenant le labyrinthe (.txt)"""
+        with open("cartes/{}".format(nom_du_fichier), "r") as labyrinthe_file:
+            laby = labyrinthe_file.read()
 
-		list_ligne_partie_en_cours = self.affichage.split("\n")
-		self.nbre_ligne = len(list_ligne_partie_en_cours)
+        self.affichage = laby.strip()
 
-		nbre_caractere = len(self.affichage) + 1
-		self.nbre_caractere_par_ligne = nbre_caractere // self.nbre_ligne
+        list_ligne_partie_en_cours = self.affichage.split("\n")
+        self.nbre_ligne = len(list_ligne_partie_en_cours)
 
-	
-	def __str__(self):
-		""" la fonction d'affichage pour print"""
-		return "\n{}\n".format(self.affichage)
+        nbre_caractere = len(self.affichage) + 1
+        self.nbre_caractere_par_ligne = nbre_caractere // self.nbre_ligne
+
+        self.caractere_a_ajouter = " "
 
 
-	def deplacer_roboc(self, caractere_a_ajouter, partie_gagnee, direction_distance):
-		"""Fonction qui vérifie si roboc ne sort pas des limites, qui affiche le déplacement de roboc pas à pas 
-		et qui renvoie , la partie en cours après le déplacement, le caractère à ajouter à la place de roboc
-		lors du déplacement suivant et une variable "booléen" partie_gagnee"""
-		list_chaine_avant_apres_x = self.affichage.split("X")	# on trouve roboc...
-		chaine_avant_x = list_chaine_avant_apres_x[0]
-		chaine_apres_x = list_chaine_avant_apres_x[1]
-		position_x = (len(chaine_avant_x) + 1) % (self.nbre_caractere_par_ligne) # ...et on lui donne...
-		position_y = (len(chaine_avant_x) + 1) // (self.nbre_caractere_par_ligne) + 1 #... une position xy
+    def __str__(self):
+        """fonction d'affichage du labyrinthe -> print(self)"""
+        return "\n{}\n".format(self.affichage)
 
-		if direction_distance[0] == "S": # on trouve la future position...
-			nvelle_position = (position_x, position_y + direction_distance[1])
-		elif direction_distance[0] == "N": #...après le déplacement...
-			nvelle_position = (position_x, position_y - direction_distance[1])
-		elif direction_distance[0] == "O": #...pour chacune des directions
-			nvelle_position = (position_x - direction_distance[1], position_y)
-		elif direction_distance[0] == "E":
-			nvelle_position = (position_x + direction_distance[1], position_y)
-		
-		if nvelle_position[0] <= 0 or nvelle_position[0] >= self.nbre_caractere_par_ligne or nvelle_position[1] <= 0 or nvelle_position[1] > self.nbre_ligne: # Attention joueur sans limite, sûrement un rebel
-			print("\nAttention!!! avec une telle distance dans cette direction, Roboc sort des limites de jeu.\n")
-			print(self, "\n")
-			return (self, caractere_a_ajouter, partie_gagnee)
-		else:
-			caractere_a_ajouter_debut = caractere_a_ajouter
-			i = 1
-			deplacement_affichage = self.affichage	
-			while i <= direction_distance[1]:
-				if direction_distance[0] == "E": # déplacement vers la droite
-					caractere_a_remplacer = chaine_apres_x[0]
-					chaine_avant_x = chaine_avant_x + caractere_a_ajouter
-					chaine_apres_x = chaine_apres_x[1:]	
-				elif direction_distance[0] == "O": # déplacement vers la gauche
-					caractere_a_remplacer = chaine_avant_x[-1]
-					chaine_avant_x = chaine_avant_x[0:-1]
-					chaine_apres_x = caractere_a_ajouter + chaine_apres_x	
-				elif direction_distance[0] == "S": # déplacement vers le bas
-					caractere_a_remplacer = chaine_apres_x[self.nbre_caractere_par_ligne-1]
-					chaine_avant_x = chaine_avant_x + caractere_a_ajouter + chaine_apres_x[0:self.nbre_caractere_par_ligne-1]
-					chaine_apres_x = chaine_apres_x[self.nbre_caractere_par_ligne:]	
-				elif direction_distance[0] == "N": # déplacement vers le haut
-					caractere_a_remplacer = chaine_avant_x[-self.nbre_caractere_par_ligne]
-					chaine_apres_x = chaine_avant_x[(len(chaine_avant_x)-self.nbre_caractere_par_ligne)+1:] + caractere_a_ajouter + chaine_apres_x
-					chaine_avant_x = chaine_avant_x[:-self.nbre_caractere_par_ligne]	
 
-				if caractere_a_remplacer == "O": # Attention joueur qui croit pouvoir passer à travers les murs
-					print("\nAttention!!! Roboc n'est pas un fantôme mais un robot, il ne traverse pas les murs.\nRetour au point de départ!!!\n")
-					print(self)
-					return (self, caractere_a_ajouter_debut, partie_gagnee)
-				elif caractere_a_remplacer == "U":
-					if i == direction_distance[1]: # Enfin un joueur digne de ce superbe jeu
-						deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
-						print("\n")
-						print(deplacement_affichage)
-						print("\n!!! GAGNE !!!".center(30))
-						partie_gagnee = True
-						self.affichage = deplacement_affichage
-						return (self, caractere_a_ajouter, partie_gagnee)
-					else: # Attention joueur aveugle qui passe devant la sortie sans s'arrêter
-						deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
-						print("\n")
-						print(deplacement_affichage)
-						caractere_a_ajouter = "U"
-						i += 1	
-				elif caractere_a_remplacer == ".": # Attention que le joueur n'embarque pas la porte
-					deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
-					print("\n")
-					print(deplacement_affichage)
-					caractere_a_ajouter = "."
-					i += 1
-				else: # La voie est libre				
-					deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
-					print("\n")
-					print(deplacement_affichage)
-					caractere_a_ajouter = " "
-					i += 1 	
+    def deplacer_roboc(self, direction_distance):
+        """fonction qui déplace Roboc (X) dans le labyrinthe
+        prenant en paramètre la direction et la distance
+        en vérifiant qu'il:
+        - ne sort pas des limites du jeu
+        - ne traverse pas les murs dans le jeu (O)
+        qui affiche son déplacement pas à pas
+        et qui renvoie True ou False à la fin du déplacement
+        selon que la partie soit gagnée ou non"""
+        list_chaine_avant_apres_x = self.affichage.split("X")    # on trouve roboc...
+        chaine_avant_x = list_chaine_avant_apres_x[0] #...dans le labyrinthe...
+        chaine_apres_x = list_chaine_avant_apres_x[1] #...et on lui donne...
+        position_x = (len(chaine_avant_x) + 1) % (self.nbre_caractere_par_ligne) #...une position x...
+        position_y = (len(chaine_avant_x) + 1) // (self.nbre_caractere_par_ligne) + 1 #...et une position y
 
-			self.affichage = deplacement_affichage
-			return (self, caractere_a_ajouter, partie_gagnee)
+        if direction_distance[0] == "S": # on trouve la future position...
+            nvelle_position = (position_x, position_y + direction_distance[1])
+        elif direction_distance[0] == "N": #...après le déplacement...
+            nvelle_position = (position_x, position_y - direction_distance[1])
+        elif direction_distance[0] == "O": #...en fonction de la direction...
+            nvelle_position = (position_x - direction_distance[1], position_y)
+        elif direction_distance[0] == "E": #...et de la distance choisies
+            nvelle_position = (position_x + direction_distance[1], position_y)
+
+        if nvelle_position[0] <= 0 or nvelle_position[0] >= self.nbre_caractere_par_ligne or nvelle_position[1] <= 0 or nvelle_position[1] > self.nbre_ligne: # Attention joueur sans limite, sûrement un rebel
+            print("\nAttention!!! avec une telle distance dans cette direction, Roboc sort des limites de jeu.\n")
+            print(self, "\n")
+            return False
+        else:
+            caractere_a_ajouter_debut_deplacement = self.caractere_a_ajouter
+            i = 1
+            deplacement_affichage = self.affichage
+            while i <= direction_distance[1]:
+                if direction_distance[0] == "E": # déplacement vers la droite
+                    caractere_a_remplacer = chaine_apres_x[0]
+                    chaine_avant_x = chaine_avant_x + self.caractere_a_ajouter
+                    chaine_apres_x = chaine_apres_x[1:]
+                elif direction_distance[0] == "O": # déplacement vers la gauche
+                    caractere_a_remplacer = chaine_avant_x[-1]
+                    chaine_avant_x = chaine_avant_x[0:-1]
+                    chaine_apres_x = self.caractere_a_ajouter + chaine_apres_x
+                elif direction_distance[0] == "S": # déplacement vers le bas
+                    caractere_a_remplacer = chaine_apres_x[self.nbre_caractere_par_ligne-1]
+                    chaine_avant_x = chaine_avant_x + self.caractere_a_ajouter + chaine_apres_x[0:self.nbre_caractere_par_ligne-1]
+                    chaine_apres_x = chaine_apres_x[self.nbre_caractere_par_ligne:]
+                elif direction_distance[0] == "N": # déplacement vers le haut
+                    caractere_a_remplacer = chaine_avant_x[-self.nbre_caractere_par_ligne]
+                    chaine_apres_x = chaine_avant_x[(len(chaine_avant_x)-self.nbre_caractere_par_ligne)+1:] + self.caractere_a_ajouter + chaine_apres_x
+                    chaine_avant_x = chaine_avant_x[:-self.nbre_caractere_par_ligne]
+
+                if caractere_a_remplacer == "O": # Attention joueur qui croit pouvoir passer à travers les murs
+                    print("\nAttention!!! Roboc n'est pas un fantôme mais un robot, il ne traverse pas les murs.\nRetour au point de départ!!!\n")
+                    self.caractere_a_ajouter = caractere_a_ajouter_debut_deplacement
+                    print(self) # Roboc remit à sa position avant le déplacement
+                    return False
+                elif caractere_a_remplacer == "U":
+                    if i == direction_distance[1]: # Enfin un joueur digne de ce superbe jeu
+                        deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
+                        print("\n")
+                        print(deplacement_affichage)
+                        print("\n!!! GAGNE !!!\n".center(30))
+                        self.affichage = deplacement_affichage
+                        return True
+                    else: # Attention joueur aveugle qui passe devant la sortie sans s'arrêter
+                        deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
+                        print("\n")
+                        print(deplacement_affichage)
+                        self.caractere_a_ajouter = "U"
+                        time.sleep(1)
+                        i += 1
+                elif caractere_a_remplacer == ".": # Attention que le joueur n'embarque pas la porte
+                    deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
+                    print("\n")
+                    print(deplacement_affichage)
+                    self.caractere_a_ajouter = "."
+                    time.sleep(1)
+                    i += 1
+                else: # La voie est libre
+                    deplacement_affichage = chaine_avant_x + "X" + chaine_apres_x
+                    print("\n")
+                    print(deplacement_affichage)
+                    self.caractere_a_ajouter = " "
+                    time.sleep(1)
+                    i += 1
+
+            self.affichage = deplacement_affichage
+            return False
--- roboc.py
+++ roboc.py
@@ -1,24 +1,30 @@
 from fonctions import *
 from classes import *
-from cartes import carte_dispo 
+from cartes import carte_dispo
 
-print("\n Aide Roboc à sortir...\n ... du labyrinthe : \n") # message de début
+quitter_partie = False
+while quitter_partie == False:
+    print("\n Aide Roboc à sortir...\n ... du labyrinthe :")
+    for i, elt in enumerate(carte_dispo.list_cartes):  # affichage de la liste des cartes dispos
+        print("\n{} - {}".center(15).format(i+1, elt[0:-4].capitalize()))
 
-for i, elt in enumerate(carte_dispo.list_cartes):  # affichage de la liste des cartes dispos
-	print("{} - {}".center(15).format(i+1, elt[0:-4].capitalize()),"\n")
+    laby_choisi = choix_laby(carte_dispo.list_cartes) # le joueur choisit le labyrinthe...
+    partie_en_cours = Partie(laby_choisi) # ... création de la partie en fonction du choix du labyrinthe -> classe Partie
+    print(partie_en_cours) # ... affichage du labyrinthe choisi
 
-laby_choisi = choix_laby(carte_dispo.list_cartes) # le joueur choisit le labyrinthe... 
+    partie_gagnee = False
+    while partie_gagnee == False:
+        direction_distance = choix_direction_distance() # le joueur choisit sa direction et sa distance
+        if direction_distance[0] == "Q": # le joueur choisit de quitter le jeu
+            quitter_partie = True
+            break
+        else:
+            partie_gagnee = partie_en_cours.deplacer_roboc(direction_distance) # ça joue!!!
 
-partie_en_cours = Carte(laby_choisi) # ... création en fonction du choix de la partie -> classe Carte
-print(partie_en_cours) # ... affichage du labyrinthe
+    if partie_gagnee == True: # C'est gagné!!! on demande au joueur s'il veut rejouer ou quitter
+        rejouer_quitter = input("q pour (Q)uitter et ce que tu veux pour rejouer: ")
+        if rejouer_quitter.upper() == "Q": # il en a marre et quitte le jeu
+            quitter_partie = True
 
-partie_gagnee = False
-caractere_a_ajouter = " "
-while partie_gagnee == False:
-	direction_distance = choix_direction_distance() # le joueur choisit sa direction et sa distance
-	partie = partie_en_cours.deplacer_roboc(caractere_a_ajouter, partie_gagnee, direction_distance) # roboc se déplacer
-	partie_en_cours = partie[0]
-	partie_gagnee = partie[2]
-	caractere_a_ajouter = partie[1]
-else:
-	print("c'est fini!!!") # c'est gagné
+print("\nBye!!! Bye!!! Bye!!!")
+
